4b0a78561d9e7f61021cda87ab210c28eb1924a0
nayeem-miah <nayeem5113a@gmail.com>
2024-11-03 11:15:49 +0600

    outlet Set dashboard

--- client/src/Pages/Dashboard/Dashboard.tsx
+++ client/src/Pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard: React.FC = () => {
     return (
         <div>
             <h3>Dashboard</h3>
+            <Outlet />
         </div>
     );
 };
