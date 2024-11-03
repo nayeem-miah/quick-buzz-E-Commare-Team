e6e49704a06b4b20a1c7e877fb46448766cdfa54
Rakibul hasan <rakibulhasan3929@gmail.com>
2024-11-03 10:04:37 -0800

    update code

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
+
 
 const Home: React.FC = () => {
     return (
         <div>
+            <Navbar></Navbar>
             home page
          
         </div>
