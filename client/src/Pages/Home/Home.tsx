15234d4da92be06bda149328ccd37f7e71e6b5b7
Rakibul hasan <rakibulhasan3929@gmail.com>
2024-11-04 10:14:12 -0800

    update

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -9,7 +9,8 @@ const Home: React.FC = () => {
             <Navbar></Navbar>
             <Banner></Banner>
             home page section
-           
+            Lorem ipsum dolor sit, 
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, iure!
         </div>
     );
 };
