94f8cf38f80c5684c5d2e398ab5659bf02c98335
nayeem-miah <nayeem5113a@gmail.com>
2024-11-04 12:05:49 +0600

    create firebase folder and config file

---
+++ client/src/Firebase/FireBase.config.ts
@@ -0,0 +1,12 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkGo5Vfnxdc2qsJpyPOBIYfXZFQ9uCs1c",
+  authDomain: "quick-bus-bd.firebaseapp.com",
+  projectId: "quick-bus-bd",
+  storageBucket: "quick-bus-bd.firebasestorage.app",
+  messagingSenderId: "533454081496",
+  appId: "1:533454081496:web:1509ca06a35f60634076ad",
+};
+export const app = initializeApp(firebaseConfig)
