0214128df7a24db1278cef389b8d0d13807d3856
Rakibul hasan <rakibulhasan3929@gmail.com>
2024-11-10 01:59:04 -0800

    check code

--- client/src/Shared/Navbar/Navbar.tsx
+++ client/src/Shared/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="relative bg-white shadow dark:bg-gray-800">
+    <nav className="relative bg-[#5eaaf5]  shadow dark:bg-gray-800">
       <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
         <div className="flex items-center justify-between">
           <a href="#">
@@ -34,40 +34,29 @@ const Navbar: React.FC = () => {
           } absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:block`}
         >
           <div className="flex flex-col md:flex-row md:mx-6">
-            <a className="my-2 text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
-              Home
-            </a>
-            <Link className="my-2 text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
-              Shop
+            <Link to='/' className="my-2 text-white hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
+              HOME
             </Link>
-            <Link to='/contact' className="my-2 text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
-              Contact
+            <Link to='/about'  className="my-2 text-white hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
+              ABOUT
             </Link>
-            <Link  className="my-2 text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
-              About
+            <Link to='/product' className="my-2 text-white hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
+              PRODUCT
             </Link>
-            <Link to='/singup'  className="my-2 text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
-              SingUp
+            <Link to='/contact' className="my-2 text-white hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
+              CONTACT
             </Link>
-            <Link to='/login'  className="my-2 text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
-              Singin
+            <Link to='/login'  className="my-2 text-white hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
+              LOGIN
             </Link>
+            <Link to='/signup'  className="my-2 text-white hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">
+              SIGN UP
+            </Link>
+            
+           
           </div>
 
-          {/* <div className="flex justify-center md:block">
-            <a className="relative text-gray-700 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="#">
-              <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <span className="absolute top-0 left-0 p-1 text-xs text-white bg-blue-500 rounded-full"></span>
-            </a>
-          </div> */}
+          
         </div>
       </div>
     </nav>
