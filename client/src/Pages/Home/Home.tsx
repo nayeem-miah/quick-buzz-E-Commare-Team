43684dbfff82f4fddf7ec71f70af558db1f905f8
Rakibul hasan <rakibulhasan3929@gmail.com>
2024-11-10 06:57:29 -0800

    change

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -4,10 +4,11 @@ import Faq from '../../Components/Faq/Faq';
 const Home: React.FC = () => {
     return (
         <div>
-
+         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla quod asperiores commodi dicta, magnam modi adipisci. Itaque dolore assumenda dolores amet placeat unde, voluptatum, at quos alias adipisci ipsam atque?
             <Faq />
         </div>
     );
 };
 
+// 
 export default Home;
