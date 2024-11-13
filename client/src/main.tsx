90c7e0b3a9386f0697048a027f95ac317f3976ad
Rakibul hasan <rakibulhasan3929@gmail.com>
2024-11-13 08:50:18 -0800

    update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,10 +7,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routers/Router';
+import AuthProvider from './Provider/AuthProvider';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='bg-white text-black'>
-      <RouterProvider router={router} />
+     <AuthProvider>
+     <RouterProvider router={router} />
+     </AuthProvider>
     </div>
   </StrictMode>,
 )
