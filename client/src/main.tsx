2137d9669f472031bd5d0f30052504b99d001b76
nayeem-miah <nayeem5113a@gmail.com>
2024-11-14 18:52:31 +0600

    marge code in firebase

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,35 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-import {
-
-  RouterProvider,
-} from "react-router-dom";
-import router from './Routers/Router';
-import AuthProvider from './Provider/AuthProvider';
-createRoot(document.getElementById('root')!).render(
+import { RouterProvider } from "react-router-dom";
+import router from "./Routers/Router";
+import AuthProvider from "./Provider/AuthProvider";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<<<<<<< HEAD
-    <div className='bg-white text-black'>
-     <AuthProvider>
-     <RouterProvider router={router} />
-     </AuthProvider>
-    </div>
-=======
     <QueryClientProvider client={queryClient}>
-      <div className='bg-white text-black'>
-        <RouterProvider router={router} />
+      <div className="bg-white text-black">
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
       </div>
     </QueryClientProvider>
->>>>>>> f0087dd2d2a1a2761ef0f33bb8a9080ea4922d03
-  </StrictMode>,
-)
-
-
+  </StrictMode>
+);
