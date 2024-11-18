65b16d87e8afd08b61a53a600daf5011465afcce
nayeem-miah <nayeem5113a@gmail.com>
2024-11-18 22:22:21 +0600

    user dashboard work start

--- client/src/Pages/Dashboard/Dashboard.tsx
+++ client/src/Pages/Dashboard/Dashboard.tsx
@@ -18,7 +18,7 @@ const Sidebar: React.FC = () => {
   const { logOut } = useAuth();
   const [isActive, setActive] = useState(false);
 
-  const role: string = "user";
+  const role: string = "admin";
   // Sidebar Responsive Handler
   const handleToggle = () => {
     setActive(!isActive);
@@ -280,8 +280,8 @@ const Sidebar: React.FC = () => {
                   <MdHomeWork className="w-5 h-5" />
                   <span className="mx-4 font-medium">My Listings</span>
                 </NavLink>
-                 {/* Add Room */}
-                 <NavLink
+                {/*my-payment-history  */}
+                <NavLink
                   to="my-payment-history"
                   className={({ isActive }) =>
                     `flex items-center px-4 py-2 my-5 transition-colors duration-300 transform hover:bg-gray-300 hover:text-gray-700 ${
