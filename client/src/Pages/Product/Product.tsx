e0ac1e491f357a7c6ec1e9d19224a37b141281cc
Rakibul hasan <rakibulhasan3929@gmail.com>
2024-11-18 05:33:47 -0800

    update code

--- client/src/Pages/Product/Product.tsx
+++ client/src/Pages/Product/Product.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import LoadingSpinner from '../../Shared/Loading';
+
+interface Card {
+  id: number;
+  name: string;
+  price: number;
+  photo: string;
+}
 
 const Product: React.FC = () => {
-    return (
-        <div>
-            <h3> Product</h3>
+  const [cards, setCard] = useState<Card[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('./menu.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setCard(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error('Error fetching menu.json:', err);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(cards);
+
+  if (loading) return <LoadingSpinner />;
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {cards.map((card) => (
+        <div
+          key={card.id}
+          className="flex flex-col items-center justify-center w-full max-w-sm mx-auto"
+        >
+          <div
+            className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md"
+            style={{
+              backgroundImage: `url(${card.photo})`,
+            }}
+          ></div>
+
+          <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+            <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
+              {card.name}
+            </h3>
+
+            <div className="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+              <span className="font-bold text-gray-800 dark:text-gray-200">
+                ${card.price}
+              </span>
+              <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">
+                Add to cart
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Product;
