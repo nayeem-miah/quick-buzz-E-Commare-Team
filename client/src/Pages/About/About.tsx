e1ba510c141f532d4f9edb16e3cbb7d4f391cb2e
MD Nayeem Miah <nayeem5113a@gmail.com>
2024-11-20 16:27:48 +0600

    creating branch

--- client/src/Pages/About/About.tsx
+++ client/src/Pages/About/About.tsx
@@ -5,6 +5,7 @@ const About: React.FC = () => {
         <div className='py-24'>
             About page
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo magnam harum saepe molestias sed? Placeat vitae unde corrupti saepe pariatur vel laborum fugiat facere! Illo recusandae beatae laborum sapiente placeat ut repudiandae perspiciatis corrupti commodi quod illum dolor possimus voluptas maxime quidem id, ratione alias eius neque amet vero, ducimus, porro doloremque? Ab id nesciunt, suscipit deserunt dolorem neque incidunt molestiae reiciendis. Eos, omnis id. Eos dolorem culpa, magni veniam asperiores voluptas dolores alias perferendis quibusdam officia deleniti aperiam fugiat, saepe a porro pariatur ratione enim accusamus quaerat explicabo nobis sapiente. Veritatis iusto aperiam laborum. Explicabo voluptates laboriosam omnis nulla.
+            work on nayeem 2g   
 
         </div>
     );
