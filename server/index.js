3eaaff465811eb8bc5e296bb1a7911e597367bcc
nayeem-miah <nayeem5113a@gmail.com>
2024-11-22 18:53:34 +0600

    backend start

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // const userCollection=
     // await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
