20d341ca3aacc2cbcc39f0072428596e67723d6d
nayeem-miah <nayeem5113a@gmail.com>
2024-11-24 18:15:53 +0600

    get all product in db

--- client/src/Components/Dashboard/Host/MyAddedProduct.tsx
+++ client/src/Components/Dashboard/Host/MyAddedProduct.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import useAxiosPublic from "../../../Hooks/UsePublic";
+import { useQuery } from "@tanstack/react-query";
 
-const MyAddedProduct : React.FC= () => {
-    return (
-        <div>
-            <h3>My add Product</h3>
-        </div>
-    );
+const MyAddedProduct: React.FC = () => {
+  const axiosPublic = useAxiosPublic();
+  const { data } = useQuery({
+    queryKey: ["allProduct"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/products");
+      return res.data;
+    },
+  });
+
+  console.log(data);
+  return (
+    <div>
+      <h3>My add Product {data?.length}</h3>
+    </div>
+  );
 };
 
 export default MyAddedProduct;
