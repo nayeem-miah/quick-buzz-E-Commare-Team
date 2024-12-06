e66c20161245a6091dcde30d1c3b62cc21769e57
Rakibul hasan <rakibulhasan3929@gmail.com>
2024-12-06 03:15:37 -0800

    update code

--- client/src/Pages/Dashboard/Dashboard.tsx
+++ client/src/Pages/Dashboard/Dashboard.tsx
@@ -306,7 +306,7 @@ const Sidebar: React.FC = () => {
                 </NavLink>
 
                 {/* my save data  */}
-                <NavLink
+                {/* <NavLink
                   to="my-wishlist"
                   className={({ isActive }) =>
                     `flex items-center px-4 py-2 my-5 transition-colors duration-300 transform hover:bg-gray-300 hover:text-gray-700 ${
@@ -316,7 +316,7 @@ const Sidebar: React.FC = () => {
                 >
                   <FaHeart className="w-5 h-5" />
                   <span className="mx-4 font-medium">My WishList</span>
-                </NavLink>
+                </NavLink> */}
                 {/*my-payment-history  */}
                 <NavLink
                   to="my-payment-history"
--- client/src/Pages/Product/ProductDetailsPage.tsx
+++ client/src/Pages/Product/ProductDetailsPage.tsx
@@ -6,13 +6,10 @@ import BannerDetailsPage from "../../Shared/Heading/BannerDetailsPage";
 import toast from "react-hot-toast";
 import useAuth from "../../Hooks/UseAuth";
 
-
 const ProductPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const axiosPublic = useAxiosPublic();
- const {user} = useAuth();
-
-  
+  const { user } = useAuth();
 
   const {
     data: product,
@@ -42,13 +39,10 @@ const ProductPage: React.FC = () => {
     discount,
   } = product;
 
-
-
-   /* data post  */
-   const HandleButton = () => {
+  /* data post  */
+  const HandleButton = () => {
     try {
       const newData = {
-       
         productImage,
         description,
         brandName,
@@ -58,35 +52,39 @@ const ProductPage: React.FC = () => {
         discount,
         email: user?.email,
       };
-  
-      console.log("Sending data:", newData); 
-  
-      axiosPublic.post('/allsave', newData).then((res) => {
-        if (res.status === 201) {
-          toast.success("Your data is saved. Please explore my listing page.");
-        } else {
-          toast.error("Failed to save data.");
-        }
-      }).catch((error) => {
-        console.error("Error posting data:", error);
-        toast.error("Server error occurred.");
-      });
+
+      console.log("Sending data:", newData);
+
+      axiosPublic
+        .post("/allsave", newData)
+        .then((res) => {
+          if (res.status === 201) {
+            toast.success(
+              "Your data is saved. Please explore my listing page."
+            );
+          } else {
+            toast.error("Failed to save data.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error posting data:", error);
+          toast.error("Server error occurred.");
+        });
     } catch (err) {
       console.error("Error occurred:", err);
       toast.error("Error in handling button.");
     }
   };
-  
 
   return (
     <div>
       <BannerDetailsPage
-       imageURL={productImage}
+        imageURL={productImage}
         headingText="Explore this Product."
         subheadingText="Please explore my QuickBuzz all Product and purchase your choice Product"
       ></BannerDetailsPage>
 
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full group lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full  bg-red-300 lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="grid gap-12 row-gap-8 lg:grid-cols-2">
           <div className="flex flex-col justify-center">
             <div className="max-w-xl mb-6">
@@ -99,17 +97,10 @@ const ProductPage: React.FC = () => {
                 {productTitle}
               </p>
               <p className="text-lg mt-2 font-bold text-orange-600">
-              $   {price}
+                $ {price}
               </p>
             </div>
-            <div className="flex flex-col md:flex-row md:gap-20 lg:items-end justify-end">
-              <Link
-                to={`/checkout/${_id}`}
-                className="mt-2 px-14 py-2 text-white bg-gradient-to-r from-purple-500 to-blue-500 rounded-md transition-all duration-500 ease-in-out
-               border-2 border-transparent hover:bg-indigo-600 hover:border-indigo-400 hover:shadow-[0_0_15px_3px_rgba(99,102,241,0.7)] hover:scale-105"
-              >
-                Buy Now
-              </Link>
+            <div className="">
               <button
                 onClick={HandleButton}
                 className="mt-2 px-10 py-2 text-white bg-gradient-to-r from-purple-500 to-blue-500 rounded-md transition-all duration-500 ease-in-out
@@ -121,11 +112,108 @@ const ProductPage: React.FC = () => {
           </div>
           <div>
             <img
-              className="object-cover lg:ml-40 ml-8 h-56 group-hover:scale-125 shadow-xl rounded-3xl sm:h-96"
+              className="object-cover lg:ml-40 ml-8 h-56 group-hover:scale-125 shadow-xl  sm:h-96"
               src={productImage}
               alt=""
             />
           </div>
+        </div>
+      </div>
+      {/*user review rating start*/}
+      <div className="flex   flex-col lg:w-[400px] lg:ml-[80px] mt-2 p-8 shadow-sm rounded-xl lg:p-12 dark:bg-gray-50 dark:text-gray-800">
+        <div className="flex flex-col items-center w-full">
+          <h2 className="text-3xl font-semibold text-center">
+            Your opinion matters!
+          </h2>
+          <div className="flex flex-col items-center py-6 space-y-3">
+            <span className="text-center">How was your experience?</span>
+            <div className="flex space-x-3">
+              <button
+                type="button"
+                title="Rate 1 stars"
+                aria-label="Rate 1 stars"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-10 h-10 dark:text-yellow-700"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+              </button>
+              <button
+                type="button"
+                title="Rate 2 stars"
+                aria-label="Rate 2 stars"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-10 h-10 dark:text-yellow-700"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+              </button>
+              <button
+                type="button"
+                title="Rate 3 stars"
+                aria-label="Rate 3 stars"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-10 h-10 dark:text-yellow-700"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+              </button>
+              <button
+                type="button"
+                title="Rate 4 stars"
+                aria-label="Rate 4 stars"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-10 h-10 dark:text-yellow-700"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+              </button>
+              <button
+                type="button"
+                title="Rate 5 stars"
+                aria-label="Rate 5 stars"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-10 h-10 dark:text-gray-400"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-4">
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <button
+           
+            className="mt-2 px-10 py-2 text-2xl text-white bg-gradient-to-r from-purple-500 to-blue-500 rounded-md transition-all duration-500 ease-in-out
+                    border-2 border-transparent hover:bg-indigo-600 hover:border-indigo-400 hover:shadow-[0_0_15px_3px_rgba(99,102,241,0.7)] hover:scale-105"
+          >
+            Review
+          </button>
         </div>
       </div>
     </div>
