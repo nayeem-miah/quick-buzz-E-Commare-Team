674ddf217f22ea8dfc3f71a0385123ac67412ed3
nayeem-miah <nayeem5113a@gmail.com>
2024-12-08 17:03:05 +0600

    cart dection update

--- client/src/Pages/Home/Category/CategoryBox.tsx
+++ client/src/Pages/Home/Category/CategoryBox.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import queryString from 'query-string';
+import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import queryString from "query-string";
 
 interface CategoryBoxProps {
   label: string;
-  icon: React.ElementType; 
+  icon: React.ElementType;
 }
 
 const CategoryBox: React.FC<CategoryBoxProps> = ({ label, icon: Icon }) => {
   const [params] = useSearchParams();
-  const category = params.get('category');
+  const category = params.get("category");
   const navigate = useNavigate();
 
   const handleClick = () => {
     const currentQuery = { category: label };
-  
+
     const url = queryString.stringifyUrl({
-      url: '/product',
+      url: "/product",
       query: currentQuery,
     });
-  
-    console.log("Generated URL:", url); 
-    navigate(url);  // নতুন ক্যাটাগরি নির্বাচন হলে URL আপডেট হবে
+
+    // console.log("Generated URL:", url);
+    navigate(url);
   };
-  
 
   return (
     <div
@@ -41,7 +40,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({ label, icon: Icon }) => {
         hover:text-neutral-800
         transition
         cursor-pointer ${
-          category === label && 'border-b-neutral-800 text-neutral-800'
+          category === label && "border-b-neutral-800 text-neutral-800"
         }`}
     >
       <Icon size={26} />
--- client/src/Pages/Product/Product.tsx
+++ client/src/Pages/Product/Product.tsx
@@ -62,7 +62,7 @@ const Product: React.FC = () => {
       {products.length == 0 ? (
         <NoData />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-4">
           {products.map(
             (product) =>
               // <Link to={`/product/${product.id}`} >
