a0e30ed2bab5b4ff1dc606b9ca3b8eca33ca9bb4
nayeem-miah <nayeem5113a@gmail.com>
2024-12-24 15:56:50 +0600

    dynamic host chat in total price per days

--- client/src/Components/Dashboard/Host/HostHome.tsx
+++ client/src/Components/Dashboard/Host/HostHome.tsx
@@ -1,14 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { FaUserAlt, FaDollarSign } from "react-icons/fa";
-import { BsFillCartPlusFill, BsFillHouseDoorFill } from "react-icons/bs";
-import UseAxiosSecure from "../../../Hooks/UseAxiosSecure";
-import { dataTagSymbol, useQuery } from "@tanstack/react-query";
-import MyChartComponent from "./HostChart";
-import ApexCart from "../Admin/Chart/ChartWrapper";
-import LoadingSpinner from "../../../Shared/Loading";
+import { BsFillCartPlusFill } from "react-icons/bs";
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/UseAuth";
+import useAxiosPublic from "../../../Hooks/UsePublic";
+import EnhancedBarChart from "./Chart/EnhancedBarChart";
 
 interface StatData {
   totalSale: number;
@@ -28,69 +23,48 @@ const HostHome: React.FC = () => {
     chartData: [], // Add dummy data if needed for the chart
   };
 
-  const { email } = useAuth();
+  const { user } = useAuth();
+  const axiosSecure = useAxiosPublic();
 
-  const axiosSecure = UseAxiosSecure();
-  const { data: users = [] } = useQuery({
-    queryKey: ["alluser"],
+  // Fetch products for the specific host email
+  const { data: productsData = [] } = useQuery({
+    queryKey: ["products"], // Include email in queryKey
     queryFn: async () => {
-      const res = await axiosSecure.get("/alluser");
-      return res.data;
+      const res = await axiosSecure.get(`/host-product/${user?.email}`);
+      // return res.data;
+      console.log(res);
     },
   });
 
-  
-
-  // Fetch products for the specific host email
-  const { data: productsData  = [] } = useQuery({
-    queryKey: ["products", email], // Include email in queryKey
-    queryFn: async () => {
-      const res = await axiosSecure.get(`/host-product/${email}`);
-      // return res.data;
-      console.log(res);
-      
-    },
-  }); 
- 
-  
-
   // total price
-//  // Fetch payment history using email
-// const { data: PaymentHistoryData = [], isLoading } = useQuery({
-//   queryKey: ["PaymentHistoryData", email], // email as dependency
-//   queryFn: async () => {
-//     const res = await axiosSecure.get(`/host-payment-history/${email}`);
-//     return res.data;
-//   },
-// });
+  //  // Fetch payment history using email
+  // const { data: PaymentHistoryData = [], isLoading } = useQuery({
+  //   queryKey: ["PaymentHistoryData", email], // email as dependency
+  //   queryFn: async () => {
+  //     const res = await axiosSecure.get(`/host-payment-history/${email}`);
+  //     return res.data;
+  //   },
+  // });
 
-// // Filter for successful payments made to this host email
-// const successfulPayments = PaymentHistoryData.filter(
-//   (item: { status: string; hostEmail: string[] }) =>
-//     item.status === "success" && item.hostEmail.some((Host) => Host === email)
-// );
-// console.log(successfulPayments);
+  // // Filter for successful payments made to this host email
+  // const successfulPayments = PaymentHistoryData.filter(
+  //   (item: { status: string; hostEmail: string[] }) =>
+  //     item.status === "success" && item.hostEmail.some((Host) => Host === email)
+  // );
+  // console.log(successfulPayments);
 
+  // // Count the number of successful payments
+  // const successfulPaymentCount = successfulPayments.length;
 
-// // Count the number of successful payments
-// const successfulPaymentCount = successfulPayments.length;
+  // // Calculate the total amount of successful payments
+  // const totalAmount = successfulPayments.reduce(
+  //   (total: number, item: { totalPrice: number }) => total + item.totalPrice,
+  //   0
+  // );
 
-// // Calculate the total amount of successful payments
-// const totalAmount = successfulPayments.reduce(
-//   (total: number, item: { totalPrice: number }) => total + item.totalPrice,
-//   0
-// );
-
-// // Output
-// console.log("Successful Payment Count:", successfulPaymentCount);
-// console.log("Total Amount of Successful Payments:", totalAmount);
-
-
-
-
-
-
-
+  // // Output
+  // console.log("Successful Payment Count:", successfulPaymentCount);
+  // console.log("Total Amount of Successful Payments:", totalAmount);
 
   return (
     <div>
@@ -137,7 +111,7 @@ const HostHome: React.FC = () => {
                 Total User
               </p>
               <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
-                {users?.length}
+                user
               </h4>
             </div>
           </div>
@@ -148,11 +122,11 @@ const HostHome: React.FC = () => {
         <div className="mb-4 grid grid-cols-1  lg:grid-cols-2 xl:grid-cols-3">
           <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2">
             <div className="p-4 text-center ">
-              <MyChartComponent></MyChartComponent>
+              <EnhancedBarChart />
             </div>
           </div>
           <div>
-            <ApexCart  users={users}></ApexCart>
+            pi chart
           </div>
         </div>
       </div>
