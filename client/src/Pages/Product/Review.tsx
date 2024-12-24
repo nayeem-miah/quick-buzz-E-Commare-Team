0ecd2e5e88c87b7da75954432d0e44b439220fcd
Rakibul hasan <rakibulhasan3929@gmail.com>
2024-12-24 05:49:59 -0800

    change code

--- client/src/Pages/Product/Review.tsx
+++ client/src/Pages/Product/Review.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
 import useAxiosPublic from "../../Hooks/UsePublic";
 import toast from "react-hot-toast";
